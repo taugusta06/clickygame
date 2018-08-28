460684f2db32440876381270fb2ab700c194e1e8
Ty Augusta <taugusta06@gmail.com>
2018-08-28 10:41:05 -0700

    working update

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => (
+  <footer className="footer">
+    <div className="bottom">
+      Clicky Game!
+    </div>
+  </footer>
+);
+
+export default Footer;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./Header.css";
+
+const Header = props => (
+  <header className="header">
+    <h1>Clicky Game!</h1>
+    <h2>
+      Click on an image to score, but don't click on any picture twice!
+    </h2>
+  </header>
+);
+
+export default Header;
